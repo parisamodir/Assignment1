688efcb1f7be344927a8bf88f8c001341b34d4fc
parisa <parisa.modirshahla@gmail.com>
2021-09-28 13:32:39 -0400

    Import my project to git

---
+++ routes/index.js
@@ -0,0 +1,38 @@
+let express = require("express");
+let router = express.Router();
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  /* render home page. */
+
+  res.render("index", {title:'Home'});
+});
+/* GET projects page. */
+
+router.get("/projects", function (req, res, next) {
+  /* render projects page. */
+
+  res.render("pages/projects");
+});
+/* GET services page. */
+
+router.get("/services", function (req, res, next) {
+  /* render services page. */
+
+  res.render("pages/services");
+});
+/* GET contactme page. */
+
+router.get("/contactme", function (req, res, next) {
+  /* render contactme page. */
+
+  res.render("pages/contactme");
+});
+/* GET aboutme page. */
+
+router.get("/aboutme", function (req, res, next) {
+  /* render aboutme page. */
+
+  res.render("pages/aboutme");
+});
+module.exports = router;
